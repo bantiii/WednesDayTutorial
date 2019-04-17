95e05edd3edf24514b892582b2ef9981cc7d7823
ethiobanjob@gmail.com <Ethiobanjob@gmail.com>
2019-04-17 13:21:30 -0700

    added a new class

---
+++ WednesDay/src/wednesdayprogram/Wednesday.java
@@ -0,0 +1,18 @@
+package wednesdayprogram;
+
+public class Wednesday {
+
+	public static void main(String[] args) {
+		String Name = "Bantamelak";
+		char[] charone = Name.toLowerCase().toCharArray();
+		for(int i=0; i<=charone.length; i++){
+			for(int j=i+1; j<=charone.length; j++){
+				if(charone[i] == charone[j]){
+					System.out.println("the duplicated value is then"+ i);
+					
+				}
+			}
+		}
+	}
+
+}
---
+++ WednesDay/src/wednesdayprogram/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author bantt
+ *
+ */
+package wednesdayprogram;
